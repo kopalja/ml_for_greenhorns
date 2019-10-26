024a3d1f6442fc6d9977dad8fe393e80076d6047
kopalja <kopal.jakub@gmail.com>
2019-10-26 14:39:42 +0200

    logistic regression done

--- labs/03/logistic_regression_sgd.py
+++ labs/03/logistic_regression_sgd.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+#dd7e3410-38c0-11e8-9b58-00505601122b
+#6e14ef6b-3281-11e8-9de3-00505601122b
+
 import argparse
 import sys
 
@@ -7,6 +11,29 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+
+
+def model(weights, inpt):
+    return 1 / (1 + np.exp(- np.dot(weights, inpt)))
+
+def CrossEntropy(prediction, y):
+    if y == 1:
+      return -np.log(prediction)
+    else:
+      return -np.log(1 - prediction)
+
+def batch_update(features, labels, weights, lr):
+    predictions = np.array([model(weights, f) for f in features])
+    gradient = np.dot(np.matrix.transpose(features),  predictions - labels)
+    return weights - lr * gradient / len(features)
+
+def mean(x):
+    return sum(x) / len(x)
+
+def loss_acc(features, targets):
+    loss = [CrossEntropy(model(weights, feature), target)for feature, target in zip(features, targets)]
+    acc = [1 if round(model(weights, feature)) == target else 0 for feature, target in zip(features, targets)]
+    return mean(loss), mean(acc)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -34,26 +61,38 @@ if __name__ == "__main__":
     train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
         data, target, test_size=args.test_ratio, random_state=args.seed)
 
+
+
     # Generate initial linear regression weights
     weights = np.random.uniform(size=train_data.shape[1])
 
     for iteration in range(args.iterations):
         permutation = np.random.permutation(train_data.shape[0])
-
+        
         # TODO: Process the data in the order of `permutation`.
         # For every `args.batch_size`, average their gradient, and update the weights.
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
+        for i in range(train_data.shape[0] // args.batch_size):
+            index = i * args.batch_size
+            weights = batch_update(
+                train_data[permutation[index:index+args.batch_size]], 
+                train_target[permutation[index:index+args.batch_size]], 
+                weights, 
+                args.learning_rate
+            )
 
         # TODO: For every episode, measure the average loss and accuracy for both the
         # train test and the test set. The loss is the average MLE loss (i.e., the
         # negative log likelihood, or crossentropy loss, or KL loss) per example.
         # Print the accuracies in percentages.
+        train_loss, train_acc = loss_acc(train_data, train_target)
+        test_loss, test_acc = loss_acc(test_data, test_target)
         print("After iteration {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
             iteration + 1,
-            # The average train loss,
-            100 * # Training accuracy,
-            # The average test loss,
-            100 * # Test accuracy,
+            train_loss,  # The average train loss,
+            100 * train_acc,  # Training accuracy,
+            test_loss, # The average test loss,
+            100 * test_acc # Test accuracy,
         ))
 
         if args.plot:
